03cab6fb529ca83f4df18703c2a59392508c55e5
George Vaz <georgebada@hotmail.com>
2023-11-26 12:51:38 -0300

    Calcule a face média, ache os valores de U, S e V

--- Eigenfaces.py
+++ Eigenfaces.py
@@ -6,7 +6,7 @@ import os
 import cv2
 from PIL import Image
 
-# %%
+
 def get_imagens(endereco, files, shape, cor = True, corte = (0,0)):
 
     num_photos = len(files)
@@ -27,6 +27,7 @@ def get_imagens(endereco, files, shape, cor = True, corte = (0,0)):
     for i, file in enumerate(files):
         
         imagem = Image.open(f'{endereco}\\{file}')
+        if not cor: imagem = imagem.convert('L')
         imagem_numpy = np.array(imagem)
 
         imagem_numpy = imagem_numpy[x1:x2,y1:y2]    # Corte da imagem original
@@ -35,7 +36,6 @@ def get_imagens(endereco, files, shape, cor = True, corte = (0,0)):
 
     return imagens
 
-# %%
 def proporcao(tamanho):
     x = tamanho**(1/2)
     x = round(x)
@@ -44,7 +44,6 @@ def proporcao(tamanho):
     y = int(tamanho/x)
     return x, y
 
-# %%
 def show_photos(imagens, cmap ='gray'):
 
     tamanho = imagens.shape[0]
@@ -57,33 +56,15 @@ def show_photos(imagens, cmap ='gray'):
     # Iterar sobre as imagens e exibi-las em sequência
     for i in range(dim_y):
         for j in range(dim_x):
-            axs[i,j].imshow(imagens[i*dim_x+j], cmap = cmap)
+            axs[i,j].imshow(imagens[i*dim_x+j], cmap=cmap)
             axs[i,j].axis('off')  # Desativar eixos para uma melhor visualização
 
     # Exibir a sequência de imagens
     plt.show()
 
-
-# %%
-endereco = "photos"
-lista_de_fotos = os.listdir(endereco)
-
-imagens_0 = get_imagens(endereco, lista_de_fotos, (775, 775), corte = (340,25))
-
-show_photos(imagens_0)
-
-# %%
-endereco = ".\\archive"
-lista_de_fotos = os.listdir(endereco)
-
-imagens = get_imagens(endereco, lista_de_fotos[2:], (243,320), cor = False)
-
-show_photos(imagens)
-
-# %%
 def create_photo(imagens, lin, col, cor = True):
 
-    
+
     altura = imagens.shape[1]
     largura = imagens.shape[2]
 
@@ -99,51 +80,78 @@ def create_photo(imagens, lin, col, cor = True):
 
     for i in range(lin):
         for j in range(col):
-            
+
             index = i*col+j
 
             foto_selecionada = imagens[index]
-        
+
             if cor:
-                imagem[i * altura : (i + 1) * altura, j * largura : (j + 1) * largura, :] = foto_selecionada 
+                imagem[i * altura : (i + 1) * altura, j * largura : (j + 1) * largura, :] = foto_selecionada
             else:
-                imagem[i * altura : (i + 1) * altura, j * largura : (j + 1) * largura] = foto_selecionada 
-            
+                imagem[i * altura : (i + 1) * altura, j * largura : (j + 1) * largura] = foto_selecionada
+
     return imagem
 
-# %%
-foto = create_photo(imagens, 15, 11, cor = False)
-plt.axis('off')
-plt.imshow(foto, cmap="gray")
-plt.show()
+def images_vector(imagens):
+    num_imagens, altura, largura = imagens.shape
 
-foto = create_photo(imagens_0, 8, 4)
-img = plt.imshow(foto)
-plt.axis('off')
-plt.show()
+    tam_col = largura * altura
 
-# %%
+    vectors = np.zeros((tam_col, num_imagens))
 
-# plt.rcParams['figure.figsize'] = [10, 10]
-# plt.rcParams.update({'font.size': 18})
+    for i in range(num_imagens):
+        vectors[:,i] = imagens[i].reshape(tam_col)
 
-# mat_contents = scipy.io.loadmat(os.path.join('.','allFaces.mat'))
-# faces = mat_contents['faces']
-# m = int(mat_contents['m'])
-# n = int(mat_contents['n'])
-# nfaces = пр.ndarray.flatten(mat_contents['nfaces'])
+    return vectors, altura, largura
 
-# allPersons = np.zeros ((n*6, m*6))
-# count = 0
+def vector_image(vector, altura, largura):
 
-# for j in range(6):
-#     for k in range(6):
-#         allPersons[j*n : (j+1)*n, k*m : (k+1)*m] = np.reshape(faces[:, 1+np.sum(nfaces[1:count-1])],n,m)
-#         count += 1
+    return vector.reshape(altura,largura)
 
-# img = plt.imshow(256-allPersons )
-# img.set_cmap('gray')
+def show_avarege_image(avarege_face, altura, largura):
+    avarege_image = avarege_face.reshape(altura,largura)
+
+    plt.axis('off')
+    plt.imshow(avarege_image, cmap="gray")
+    plt.show()
+
+
+
+endereco = "photos"
+lista_de_fotos = os.listdir(endereco)
+imagens_0 = get_imagens(endereco, lista_de_fotos, (775, 775),cor = False, corte = (340,25))
+#show_photos(imagens_0)
+
+
+endereco = ".\\archive"
+lista_de_fotos = os.listdir(endereco)
+imagens = get_imagens(endereco, lista_de_fotos[2:], (243,320), cor = False)
+#show_photos(imagens)
+
+def treino(imagens, num_treino):
+    imagens_vetor, altura, largura = images_vector(imagens)
+    imagens_treino = imagens_vetor[:,num_treino:]
+
+    avarege_face = np.mean(imagens_treino, axis=1)
+    show_avarege_image(avarege_face, altura, largura)
+
+    X = (imagens_treino.transpose() - avarege_face).transpose()
+
+    U, S, V = np.linalg.svd(X, full_matrices=False)
+
+treino(imagens, 10)
+treino(imagens_0, 5)
+
+
+print("FIM")
+
+# foto = create_photo(imagens, 15, 11, cor = False)
+# plt.axis('off')
+# plt.imshow(foto, cmap="gray")
+# plt.show()
+#
+#
+# foto = create_photo(imagens_0, 8, 4, cor = False)
+# img = plt.imshow(foto, cmap="gray")
 # plt.axis('off')
 # plt.show()
-
-
